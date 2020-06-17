930da0506061eef53054da9885424b1f8da2ece4
HelamanOteiza <67022301+HelamanOteiza@users.noreply.github.com>
2020-06-17 16:54:23 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## For this, is beneficts to caching the inverse of the matrix rather than compute it repeatedly.
 ## Then, we have two function that can we use to create a special object that stores a matrix and cache its inverse
 
-## Whit this function we can create an special object ("Matrix") that can stored on cache its inverse
+## With this function we can create an special object ("Matrix") that can stored on cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
